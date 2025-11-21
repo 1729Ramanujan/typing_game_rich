9e00c273852c65dbc06640a6ba38e1deabcb865a
1729Ramanujan <25sugimoto@gmail.com>
2025-11-21 12:38:52 +0900

    フォルダ整理

---
+++ js/data.js
@@ -0,0 +1,52 @@
+var bulbasaurTemplate = {
+    name: "フシギダネ",
+    maxhp: 20,
+    a: 10,
+    d: 10,
+    img: "./img/bulbasaur.png",
+    moves: [
+        { name: "たいあたり", action: (user, opponent) => tackle(user, opponent) },
+        { name: "つるぎのまい", action: (user, opponent) => swoardsdance(user, opponent) },
+        { name: "こうごうせい", action: (user, opponent) => synthesis(user, opponent) },
+        { name: "ソーラービーム", action: (user, opponent) => solarbeam(user, opponent) }
+    ]
+};
+
+var charmanderTemplate = {
+    name: "ヒトカゲ",
+    maxhp: 20,
+    a: 10,
+    d: 10,
+    img: "./img/charmander.png",
+    moves: [
+        { name: "たいあたり", action: (user, opponent) => tackle(user, opponent) },
+        { name: "つるぎのまい", action: (user, opponent) => swoardsdance(user, opponent) },
+        { name: "こうごうせい", action: (user, opponent) => synthesis(user, opponent) },
+        { name: "ソーラービーム", action: (user, opponent) => solarbeam(user, opponent) }
+    ]
+};
+function tackle(user, opponent) {
+    opponent.hp -= user.a * 2 - opponent.d;
+    if (opponent.hp <= 0) {
+        opponent.hp = 0;
+    }
+    $(".explanation").text(user.name + "のたいあたり！");
+}
+function solarbeam(user, opponent) {
+    opponent.hp -= user.a * 2 - opponent.d;
+    if (opponent.hp <= 0) {
+        opponent.hp = 0;
+    }
+    $(".explanation").text(user.name + "のソーラービーム！");
+}
+function swoardsdance(user, opponent) {
+    user.a = user.a * 2;
+    $(".explanation").text(user.name + "のつるぎのまい！");
+    setTimeout(() => {
+        $(".explanation").text(user.name + "のこうげきがあがった！");
+    }, 1000)
+}
+function synthesis(user, opponent) {
+    user.hp = user.hp + 5;
+    $(".explanation").text(user.name + "のこうごうせい！");
+}
